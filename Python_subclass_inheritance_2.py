512fd8f47cd5d41e28a75a40b4f6ebc1bef6e7f2
AshfaqueAlam <82508902+AshfaqueAlam@users.noreply.github.com>
2021-06-04 18:00:57 +0530

    Update Python_subclass_inheritance_2.py

--- Python_subclass_inheritance_2.py
+++ Python_subclass_inheritance_2.py
@@ -1,19 +1,25 @@
 # Single inheritance using constructor.
 
 class vehicle:    # Parent class.
-    def __init__(self,name,price):
-        self.name=name
-        self.price=price
+    def __init__(self, name, price):
+        self.name = name
+        self.price = price
+
     def display(self):
-        print("Name: ",self.name)
+        print("Name: ", self.name)
+
+
 class category(vehicle):    # Derived class / subclass.
-    def __init__(self,name,price):
-        vehicle.__init__(self,name,price)    # Pass data to base constructor.
+    def __init__(self, name, price):
+        vehicle.__init__(self, name, price)    # Pass data to base constructor.
+
     def disp_price(self):
-        print("Price: ",self.price)
-car1=category("Audi",4000000)
+        print("Price: ", self.price)
+
+
+car1 = category("Audi", 4000000)
 car1.display()
 car1.disp_price()
-car2=category("BMW",6000000)
+car2 = category("BMW", 6000000)
 car2.display()
 car2.disp_price()
