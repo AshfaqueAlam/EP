bbcf097c23ef8841fe41c4ed55b9937fde9f354e
AshfaqueAlam <82508902+AshfaqueAlam@users.noreply.github.com>
2021-06-04 18:01:57 +0530

    Update Python_multilevel_inheritance_1.py

--- Python_multilevel_inheritance_1.py
+++ Python_multilevel_inheritance_1.py
@@ -3,14 +3,19 @@
 class c1:
     def display1(self):
         print("class c1")
+
+
 class c2(c1):
     def display2(self):
         print("class c2")
+
+
 class c3(c2):
     def display3(self):
         print("class c3")
 
-s1=c3()
+
+s1 = c3()
 s1.display3()
 s1.display2()
 s1.display1()
