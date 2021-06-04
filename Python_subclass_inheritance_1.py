dae65198809422043c8e04940d0bb7832b28fa4a
AshfaqueAlam <82508902+AshfaqueAlam@users.noreply.github.com>
2021-06-04 18:01:32 +0530

    Update Python_subclass_inheritance_1.py

--- Python_subclass_inheritance_1.py
+++ Python_subclass_inheritance_1.py
@@ -1,13 +1,19 @@
 # Single inheritance without using constructor.
 
 class vehicle:    # Parent class
-    name='BMW'
+    name = 'BMW'
+
     def display(self):
-        print("Name: ",self.name)
+        print("Name: ", self.name)
+
+
 class category(vehicle):    # Derived class / subclass.
-    price=4000000
+    price = 4000000
+
     def disp_price(self):
-        print("Price: ",self.price)
-car1=category()
+        print("Price: ", self.price)
+
+
+car1 = category()
 car1.display()
 car1.disp_price()
