218c7943d8476701c65737e367d91409eddf1ce8
AshfaqueAlam <82508902+AshfaqueAlam@users.noreply.github.com>
2021-08-12 14:47:41 +0530

    Add files via upload

---
+++ StringComparison.java
@@ -0,0 +1,17 @@
+class StringComparison {
+    public static void main(String[] args) {
+        String s1 = "A";
+        String s2 = "A";
+        String s3 = "A";
+        String s4 = new String("A");
+
+        // Compare s1 & s2. Should return `true` as both refers to same object in memory.
+        System.out.println(s1 + " == " + s2 + ": " + (s1 == s2));
+
+        // Compare s1 & s3. Should return `true` as both refers to same object in memory.
+        System.out.println(s1 + " == " + s3 + ": " + (s2 == s3));
+
+        // Compare s1 & s4. Should return `false` as both refers to different object in memory.
+        System.out.println(s1 + " == " + s4 + ": " + (s1 == s4));
+    }
+}
